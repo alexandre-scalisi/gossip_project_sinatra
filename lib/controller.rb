0ddb200ce6d5011c63d6562a779d54b6feb65ef0
alexlemia13 <alexlemia13@outlook.fr>
2020-10-19 21:57:14 +0200

    fini

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,41 +1,52 @@
+# frozen_string_literal: true
+
 require 'bundler'
 require 'gossip'
 require 'csv'
 Bundler.require
 
 class ApplicationController < Sinatra::Base
- 
   get '/' do
-      erb :index, locals: {gossips: Gossip.all}
+    erb :index, locals: { gossips: Gossip.all }
   end
 
-
-  run! if app_file == $0
-
-  get '/gossips/new/' do 
+  get '/gossips/new/' do
     erb :new_gossip
   end
 
   post '/gossips/new/' do
-    Gossip.new(params["gossip_author"],params["gossip_content"]).save
+    Gossip.new(params['gossip_author'], params['gossip_content']).save
     redirect '/'
   end
 
   get '/gossips/:id' do
     int_id = params['id'].to_i
-    erb :show, locals:{gossips: Gossip.find(int_id), id: int_id}
+    erb :show, locals: { gossips: Gossip.find(int_id), id: int_id }
   end
-  
+
   get '/gossips/:id/edit/' do
-    erb :edit ,locals:{id: params["id"]}
+    erb :edit, locals: { id: params['id'] }
   end
 
   post '/gossips/:id/edit/' do
-    
-
-    Gossip.update(params["id"].to_i ,[params["gossip_author"],params["gossip_content"]])
+    Gossip.update(params['id'].to_i, [params['gossip_author'], params['gossip_content']])
     redirect '/'
   end
 
-end
+  get '/gossips/:id/comments/' do
+    erb :comment, locals: { id: params['id'] }
+  end
 
+  post '/gossips/:id/comments/' do
+    Gossip.save_comments(params['id'].to_i, "#{params['comment_author']}-----#{params['comment_content']}")
+    redirect '/'
+  end
+
+  get '/gossips/:id/show_comments/' do
+    erb :show_comments, locals: { id: params['id'], gossips: Gossip.find_comments(params['id'].to_i) }
+  end
+
+  get '/gossips/delete_all/' do
+    erb :delete_all, locals: { delete: Gossip.delete_all }
+  end
+end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,56 +1,93 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'pry'
 class Gossip
+  attr_reader :author, :content
 
-  attr_reader :author, :content 
-
-  def initialize(author,content)
+  def initialize(author, content)
     @author = author
     @content = content
   end
 
   def save
-    CSV.open("./db/gossip.csv", "ab") do |csv|
+    CSV.open('./db/gossip.csv', 'ab') do |csv|
       csv << [@author, @content]
     end
-    
+    CSV.open('./db/gossip_commentaires.csv', 'ab') do |csv|
+      csv << ['no comment']
+    end
   end
 
   def self.all
     all_gossips = []
-    CSV.read("./db/gossip.csv").each do |csv_line|
-    all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    CSV.read('./db/gossip.csv').each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
     end
 
-    return all_gossips
+    all_gossips
   end
 
   def self.find(id)
     ligne = []
-    CSV.read("./db/gossip.csv").each_with_index do |csv,i|
-      ligne = csv if i==(id-1)
+    CSV.read('./db/gossip.csv').each_with_index do |csv, i|
+      ligne = csv if i == (id - 1)
     end
-    
-    return ligne
+
+    ligne
   end
 
-  def self.update(id,info)
-    
-     arr = []
-     CSV.read("./db/gossip.csv").each do |csv_line|
+  def self.update(id, info)
+    arr = []
+    CSV.read('./db/gossip.csv').each do |csv_line|
       arr << csv_line
-     end
-     CSV.open("./db/gossip.csv","wb") do |csv|
-      
-      arr.each_with_index do |line,i|
-        if (id-1) == i
-          csv << info
-        else
-          csv << line
-        end
-        
+    end
+    CSV.open('./db/gossip.csv', 'wb') do |csv|
+      arr.each_with_index do |line, i|
+        csv << if (id - 1) == i
+                 info
+               else
+                 line
+               end
       end
     end
-    
+  end
+
+  def self.save_comments(id, infos)
+    id -= 1
+    arr = []
+    CSV.read('./db/gossip_commentaires.csv').each do |csv_line|
+      arr << csv_line
+    end
+
+    if arr[id] == ['no comment']
+      arr[id] = [infos]
+    else arr[id] << infos
+    end
+    CSV.open('./db/gossip_commentaires.csv', 'wb') do |csv|
+      arr.each do |line|
+        csv << line
+      end
+    end
+  end
+
+  def self.find_comments(id)
+    arr = []
+    index = 1
+    CSV.read('./db/gossip_commentaires.csv').each do |csv_line|
+      if index == id
+        arr << csv_line
+        return arr
+      end
+      index += 1
+    end
+    arr
+  end
+
+  def self.delete_all
+    CSV.open('./db/gossip.csv', 'wb') do |_csv|
+    end
+    CSV.open('./db/gossip_commentaires.csv', 'wb') do |_csv|
+    end
   end
 end
